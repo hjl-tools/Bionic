db879118bc42ca57aa36786529f060dec5b317de
H.J. Lu <hjl.tools@gmail.com>
2011-12-12 11:59:45 -0800

    Print out sizes of time_t, off_t, struct stat/statfs
    2011-12-12  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* test/hello.c: Include <time.h>, <sys/stat.h> and <sys/statfs.h>.
    	(hello): Print out sizes of time_t, off_t, struct stat and
    	struct statfs.

--- build/test/hello.c
+++ build/test/hello.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <sys/statfs.h>
 
 static void
 hello ()
 {
-  printf ("Hello world\n");
+  printf ("sizeof time_t: %d\n", sizeof (time_t));
+  printf ("sizeof off_t: %d\n", sizeof (off_t));
+  printf ("sizeof struct stat: %d\n", sizeof (struct stat));
+  printf ("sizeof struct statfs: %d\n", sizeof (struct statfs));
 }
 
 int
