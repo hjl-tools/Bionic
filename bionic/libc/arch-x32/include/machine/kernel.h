4c08bb9d3d7bf71e630042ae6f7e7da00eea9d56
H.J. Lu <hjl.tools@gmail.com>
2011-12-12 12:45:34 -0800

    Update x32 machine/kernel.h
    X32 uses 64bit filesystem calls.  Make sure that __kernel_blkcnt_t,
    __kernel_blksize_t, __kernel_fsblkcnt_t and __kernel_fsfilcnt_t are 64bit.

--- bionic/libc/arch-x32/include/machine/kernel.h
+++ bionic/libc/arch-x32/include/machine/kernel.h
@@ -24,18 +24,18 @@
 ** OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 ** ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-#ifndef _ARCH_X86_KERNEL_H
-#define _ARCH_X86_KERNEL_H
+#ifndef _ARCH_X32_KERNEL_H
+#define _ARCH_X32_KERNEL_H
 
 /* this file contains kernel-specific definitions that were optimized out of
    our processed kernel headers, but still useful nonetheless... */
 
-typedef unsigned long   __kernel_blkcnt_t;
-typedef unsigned long   __kernel_blksize_t;
+typedef unsigned long long   __kernel_blkcnt_t;
+typedef unsigned long long   __kernel_blksize_t;
 
 /* these aren't really defined by the kernel headers though... */
-typedef unsigned long   __kernel_fsblkcnt_t;
-typedef unsigned long   __kernel_fsfilcnt_t;
+typedef unsigned long long   __kernel_fsblkcnt_t;
+typedef unsigned long long   __kernel_fsfilcnt_t;
 typedef unsigned int    __kernel_id_t;
 
-#endif /* _ARCH_X86_KERNEL_H */
+#endif /* _ARCH_X32_KERNEL_H */
