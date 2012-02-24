45284f087249ddd80e7bdae072242307f292145a
H.J. Lu <hjl.tools@gmail.com>
2012-02-24 11:23:49 -0800

    Remove _tls_desc_lock in x32 __set_tls
    Since there is no race condition in x32 __set_tls, there is no need
    for _tls_desc_lock.

--- bionic/libc/arch-x32/bionic/__set_tls.c
+++ bionic/libc/arch-x32/bionic/__set_tls.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2012 The Android Open Source Project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,14 +25,7 @@
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-#include <pthread.h>
 #include <asm/prctl.h>
-
-static pthread_mutex_t  _tls_desc_lock = PTHREAD_MUTEX_INITIALIZER;
-
-struct _thread_area_head {
-    void *self;
-};
 
 extern int __arch_prctl(int, unsigned long *);
 
@@ -41,24 +34,7 @@ extern int __arch_prctl(int, unsigned long *);
  */
 int __set_tls(void *ptr)
 {
-    int   rc, segment;
-
-    pthread_mutex_lock(&_tls_desc_lock);
-
     /* We also need to write the location of the tls to ptr[0] */
-    ((struct _thread_area_head *)ptr)->self = ptr;
-
-    rc = __arch_prctl (ARCH_SET_FS, ptr);
-    if (rc != 0)
-    {
-        /* could not set thread local area */
-        return -1;
-    }
-
-    pthread_mutex_unlock(&_tls_desc_lock);
-
-    return 0;
+    *(void **) ptr = ptr;
+    return __arch_prctl(ARCH_SET_FS, ptr);
 }
-
-
-
